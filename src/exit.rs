b5fc2f570439f28c72d2afa3dcae03a9ad9b4129
Martin Lukas <martinlukasgit@protonmail.com>
2025-12-15 05:25:11 +0100

    Handle panic and terminal exit better

---
+++ src/exit.rs
@@ -0,0 +1,38 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{
+    io::{self, stderr, stdout, Write},
+    panic,
+};
+
+pub fn install_panic_hook() {
+    let default_hook = panic::take_hook();
+
+    panic::set_hook(Box::new(move |panic_info| {
+        let _ = disable_raw_mode();
+        let _ = execute!(stderr(), LeaveAlternateScreen, Show);
+        let _ = stderr().flush();
+
+        default_hook(panic_info);
+    }));
+}
+
+pub struct TerminalGuard;
+
+impl TerminalGuard {
+    pub fn enter() -> io::Result<Self> {
+        enable_raw_mode()?;
+        execute!(stdout(), EnterAlternateScreen, Hide)?;
+        Ok(Self)
+    }
+}
+
+impl Drop for TerminalGuard {
+    fn drop(&mut self) {
+        let _ = execute!(stdout(), LeaveAlternateScreen, Show);
+        let _ = disable_raw_mode();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 mod app;
 mod book;
 mod event_handling;
+mod exit;
 mod logging;
 mod persistance;
 mod rendering;
+mod util;
 mod view;
 
-use crate::{app::App, event_handling::handle_event, rendering::Renderer};
+use crate::{
+    app::App, event_handling::handle_event, exit::install_panic_hook, exit::TerminalGuard,
+    rendering::Renderer,
+};
 use log::info;
 use std::io;
 
 fn main() -> io::Result<()> {
+    install_panic_hook();
+    let _terminal = TerminalGuard::enter()?;
+
     logging::setup_logger().expect("Failed to setup logger");
     info!("BOOKIE STARTED");
-
-    Renderer::init_screen()?;
 
     let saved_state = persistance::load_state()?;
     let mut app = App::new(saved_state);
 
     loop {
         Renderer::render(&app)?;
-
         handle_event(&mut app)?;
-
         if app.should_quit {
             break;
         }
     }
 
     info!("BOOKIE EXITING");
-    Renderer::exit_screen()?;
     persistance::save_state(app)?;
 
     Ok(())
